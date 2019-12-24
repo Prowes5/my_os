c36a42971a384523a231257190d48655c6da0109
Prowes5 <35689411+Prowes5@users.noreply.github.com>
2019-12-24 23:29:53 +0800

    fix before bugs and add 08 day

---
+++ 08_day(04_day)/README.md
@@ -0,0 +1,3 @@
+# 08_day
+
+添加了`naskfunc.nas`，可以在这里用x86汇编语言定义函数，并在`bookpack.c`中声明调用
---
+++ 08_day(04_day)/simple_os/bootpack.c
@@ -0,0 +1,15 @@
+void io_hlt(void);
+void write_memory(int addr, int value);
+
+void HariMain(void)
+{
+	int i;
+	for(i = 0xa0000; i <= 0xaffff; i++) {
+		write_memory(i, 0xf);
+	}
+
+fin:
+	io_hlt();
+	goto fin;
+
+}
